8c75c4f5ce2a2df94340b6dbdf6f3000073242fd
M-Dung <22520284@gm.uit.edu.vn>
2025-06-02 12:47:18 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 set -e
 
-# Phân tích Internal Database URL
 export DB_USER=odoo
-export DB_PASSWORD=FAezsfGie1cYjjIiI9WXeYWQ34HQV9L3
-export DB_HOST=dpg-d0uiviidbo4c73b6ijig-a
+export DB_PASSWORD=...
+export DB_HOST=...
 export DB_PORT=5432
-export DB_NAME=odoo_w39a
+export DB_NAME=...
 
-# Cập nhật database (nếu cần)
+# Chạy cập nhật database (nếu cần)
 odoo -c /etc/odoo/odoo.conf \
   --db_host=$DB_HOST \
   --db_port=$DB_PORT \
   --db_user=$DB_USER \
   --db_password=$DB_PASSWORD \
-  -d $DB_NAME -u all --stop-after-init || true
+  -d $DB_NAME -u all --stop-after-init
 
-# Khởi động Odoo
+# Khởi động Odoo chính thức
 exec odoo -c /etc/odoo/odoo.conf \
   --db_host=$DB_HOST \
   --db_port=$DB_PORT \
